65761b53383b5c45d1e0251d60a0e03a6e249e2a
Shubham <shubhamm.0010@gmail.com>
2025-07-28 22:18:51 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -3,10 +3,22 @@ const express = require('express');
 const app = express();
 
 
-app.use("/test",(req, res) => {
-    res.send('Hello World');
+app.use("/",(req, res) => {
+    res.send('Hello World hum first');
 });
 
+app.use("/yo",(req, res) => {
+    res.send('hum first');
+});
+app.use("/wassup",(req, res) => {
+    res.send('m first');
+});
+app.use("/lessgo",(req, res) => {
+    res.send('Herst');
+});
+app.use("/hmmm",(req, res) => {
+    res.send('Hello');
+});
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
