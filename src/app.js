91f1831b53e7e1d4d43f58a975d4955b020d425f
Shubham <shubhamm.0010@gmail.com>
2025-08-29 23:00:21 +0530

    uses different route handler

--- src/app.js
+++ src/app.js
@@ -2,17 +2,31 @@ const express = require('express');
 
 const app = express();
 
-app.get('/user',(req,res)=>{
-    res.send({firstName : "Akshay",lastName:'kumar'})
-})
-app.post('/user',(req,res)=>{
-    res.send('database ')
-})
+app.use('/user',(req,res,next) =>{
+    console.log("111");
+    next()
+    res.send('1');
+},(req,res,next) =>{
+    console.log("121");
+    next()
+    res.send('1');}
+,(req,res,next) =>{
+    console.log("1581");
+    res.send('2222');
+}
+)
+
+// app.get('/user',(req,res)=>{
+//     res.send({firstName : "Akshay",lastName:'kumar'})
+// })
+// app.post('/user',(req,res)=>{
+//     res.send('database ')
+// }) 
 
 
-app.use("/test",(req, res) => {
-    res.send('Hello World hum first');
-});
+// app.use("/test",(req, res) => {
+//     res.send('Hello World hum first');
+// });
 
 
 app.listen(3000, () => {
